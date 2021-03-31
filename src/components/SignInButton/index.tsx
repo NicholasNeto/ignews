0fb860935068899ad2211b985082751db6533328
NicholasNeto <nicholasneto@outlook.com>
2021-03-30 22:35:00 -0300

    control Auth with secssion with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,20 @@
-import styles from './styles.module.scss'
-
-
+import { signIn, signOut, useSession } from 'next-auth/client'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
+import styles from './styles.module.scss'
 export function SignInButton() {
-    const isUserLoggedIn = false;
 
-    return isUserLoggedIn ? (
+    const [ session ] = useSession()
+
+    return session ? (
         <button
             type='button'
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color='#04d361' />
-        Nicholas Neto
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
 
         </button>
@@ -21,6 +22,7 @@ export function SignInButton() {
         <button
             type='button'
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#eda414' />
             Sing in with Github
