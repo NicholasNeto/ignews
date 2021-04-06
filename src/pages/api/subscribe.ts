d7ff511069de8c0abb36bc9e9e47ebd4f1081bab
NicholasNeto <nicholasneto@outlook.com>
2021-04-06 08:04:56 -0300

    created session in stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "../../services/stripe";
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
     if(request.method === 'POST'){
-
+        const checkoutSession = await stripe.checkout.sessions.create({
+            
+        })
     } else {
       response.setHeader('Allow', 'POST');
       response.status(405).end('Method not allowed');
