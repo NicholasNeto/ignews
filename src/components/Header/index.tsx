61294b11f27ded503457b44c6a0d923bceb4dbe0
NicholasNeto <nicholasneto@outlook.com>
2021-04-15 22:46:30 -0300

    used link  from next/link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,22 @@
+
+import Link from 'next/link'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 
-export function Header(){
-    return(
+export function Header() {
+    return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news"/>
+                <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active} href="home">Home</a>
-                    <a href="posts">Posts</a>
+                    <Link href="/">
+                        <a className={styles.active} href="home">Home</a>
+                    </Link>
+                    <Link href="/posts" prefetch>
+                        <a>Posts</a>
+                    </Link>
                 </nav>
-
                 <SignInButton />
             </div>
         </header>
