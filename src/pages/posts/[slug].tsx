cec78250eb5128d751e04a5861dba7773b1d246a
NicholasNeto <nicholasneto@outlook.com>
2021-04-18 17:35:22 -0300

    create styles for preview page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -47,7 +47,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     console.log("session", session)
 
 
-    if(!session.activeSubscription){
+    if(!session?.activeSubscription){
         return {
             redirect: {
                 destination: '/',
